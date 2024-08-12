23f02017482cab246ae7c073fd57e61f257d4576
anggifit <abonaldy@gmail.com>
2024-08-12 12:59:11 +0200

    Update about section's style

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'preact/hooks';
+
+export default function Greeting({messages}) {
+
+  const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
+
+  const [greeting, setGreeting] = useState(messages[0]);
+
+  return (
+    <div>
+      <h3>{greeting} ¡Gracias por tu visita!</h3>
+      <button onClick={() => setGreeting(randomMessage())}>
+        Nuevo saludo
+      </button>
+    </div>
+  );
+}
